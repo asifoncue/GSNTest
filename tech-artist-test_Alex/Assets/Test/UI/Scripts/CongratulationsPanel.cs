b8ac0e3c253c1b6ca0c4428b9815efade835050a
asifoncue <aturbyfield@gmail.com>
2021-05-25 22:39:57 -0500

    Uploading Unity files.

---
+++ tech-artist-test_Alex/Assets/Test/UI/Scripts/CongratulationsPanel.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class CongratulationsPanel : MonoBehaviour
+{
+    private int counterNum;
+    [SerializeField]
+    private GameObject[] starTweens;
+    [SerializeField]
+    private ParticleSystem starParticles;
+    [SerializeField]
+    private TextMeshProUGUI counterText;
+
+    public void StartFlyover (int tweenIndex)
+    {
+        RewardFlyOver tweenPos = starTweens[tweenIndex].GetComponent<RewardFlyOver>();
+        tweenPos?.StartFlyOver(FlyoverComplete);
+    }
+
+    private void FlyoverComplete ()
+    {
+        starParticles.Play();
+        CounterIncrement();
+    }
+
+    public void CounterIncrement()
+    {
+        counterNum++;
+        counterText.text = counterNum.ToString();
+    }
+}
---
+++ tech-artist-test_Alex/Assets/Test/UI/Scripts/RewardFlyOver.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using UnityEngine;
+using DG.Tweening;
+
+public class RewardFlyOver : MonoBehaviour
+{
+    private Vector3 startPosition;
+    private Vector3 startScale;
+
+    [SerializeField]
+    private Transform toTransform;
+
+    [SerializeField]
+    private float duration;
+
+    [SerializeField]
+    private Vector3 scaleTo;
+
+    [SerializeField]
+    private AnimationCurve easeCurveX;
+    [SerializeField]
+    private AnimationCurve easeCurveY;
+
+    public void Start()
+    {
+        startPosition = transform.localPosition;
+        startScale = transform.localScale;
+    }
+
+    public void StartFlyOver(TweenCallback callback)
+    {
+        gameObject.SetActive(true);
+        StartCoroutine(FlyOver(callback));
+    }
+
+    private IEnumerator FlyOver(TweenCallback callback)
+    {
+        Tween myTweenX = transform.DOMoveX(toTransform.position.x, duration).SetEase(easeCurveX).OnComplete(callback);
+        transform.DOMoveY(toTransform.position.y, duration).SetEase(easeCurveY);
+        transform.DOScale(scaleTo, duration);
+        yield return myTweenX.WaitForCompletion();
+        gameObject.SetActive(false);
+        ResetPosition();
+    }
+
+    public void ResetPosition()
+    {
+        transform.localPosition = startPosition;
+        transform.localScale = startScale;
+    }
+}
